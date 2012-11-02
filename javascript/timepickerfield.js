2cdd5f93cf601a993370aaaec30e998b23e71d9d
Shea <shea@silverstripe.com.au>
2012-11-02 13:55:59 +1100

    BUGFIX trigger show better

--- javascript/timepickerfield.js
+++ javascript/timepickerfield.js
@@ -1,22 +1,17 @@
 (function($) {
 
-	$('input.timepicker').timepicker({
-
-	});
-
 	$.fn.extend({
 		ssTimepicker: function(opts) {
 			return $(this).each(function() {
 				if($(this).hasClass('timepicker-applied')) return; // already applied
-				
 				$(this).addClass('timepicker-applied').timepicker(opts);
+				$(this).blur().focus(); // trigger show
 			});
 		}
 	});
 
 	$('.field.time input.text').live('click', function() {
 		$(this).ssTimepicker();
-		$(this)._showTimepicker;
 	});
 
 }(jQuery));

19700b96ca300126cc35ef2e2acc769cd4c30383
Shea <shea@livesource.co.nz>
2017-05-04 12:32:37 +1200

    fix styles for newer ss versions

--- code/DateTimePickerField.php
+++ code/DateTimePickerField.php
@@ -1,15 +1,17 @@
 <?php
 /**
  * Extends DatetimeField to use TimePickerField instead of TimeField
- * 
+ *
  * @package timepickerfield
  */
-class DatetimePickerField extends DatetimeField
-{
-        
-    public function __construct($name, $title = null, $value = "")
-    {
-        parent::__construct($name, $title, $value);
-        $this->timeField = TimepickerField::create($name . '[time]', false);
-    }
+class DatetimePickerField extends DatetimeField {
+
+	public function __construct($name, $title = null, $value = ""){
+		parent::__construct($name, $title, $value);
+		$this->setTimeField(
+			TimepickerField::create($name . '[time]', false)
+				->addExtraClass('fieldgroup-field')
+		);
+		$this->addExtraClass('datetime');
+	}
 }

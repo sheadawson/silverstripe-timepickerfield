6f63edf89f51934e3ef4c2cd5939508999a35769
Ishan Jayamanne <ishanjmails@gmail.com>
2018-11-26 01:56:20 +0000

    upgrade to ss4

--- src/TimePickerField.php
+++ src/TimePickerField.php
@@ -1,9 +1,25 @@
 <?php
+
+namespace SheaDawson\TimePickerField;
+
+use SilverStripe\Core\Convert;
+use SilverStripe\Forms\TimeField;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\View\Requirements;
+
 class TimePickerField extends TimeField
 {
 
+    /**
+     * @var array
+     */
     protected $timePickerConfig = array();
 
+    /**
+     * @param array $properties
+     *
+     * @return DBHTMLText
+     */
     public function Field($properties = array())
     {
         $this->addExtraClass('timepicker')
@@ -14,19 +30,30 @@ class TimePickerField extends TimeField
         Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery-ui.css');
         Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-ui/jquery-ui.js');
 
-        Requirements::javascript(TIMEPICKERFIELD_MODULE . '/javascript/jquery.ui.timepicker.js');
-        Requirements::javascript(TIMEPICKERFIELD_MODULE . '/javascript/timepickerfield.js');
-        Requirements::css(TIMEPICKERFIELD_MODULE . '/css/jquery.ui.timepicker.css');
+        Requirements::javascript('SheaDawson/TimePickerField:client/javascript/jquery.ui.timepicker.js');
+        Requirements::javascript('SheaDawson/TimePickerField:client/javascript/timepickerfield.js');
+        Requirements::css('SheaDawson/TimePickerField:client/css/jquery.ui.timepicker.css');
 
         return parent::Field($properties);
     }
 
+    /**
+     * @param $k
+     * @param $v
+     *
+     * @return $this
+     */
     public function setTimePickerConfig($k, $v)
     {
         $this->timePickerConfig[$k] = $v;
         return $this;
     }
 
+    /**
+     * @param null $k
+     *
+     * @return array|mixed
+     */
     public function getTimePickerConfig($k = null)
     {
         return $k ? $this->timePickerConfig[$k] : $this->timePickerConfig;
